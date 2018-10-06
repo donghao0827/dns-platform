31e31e6a713b08fdc52687bbac71e813c3f1d75f
donghao <donghao0827@outlook.com>
2018-10-06 20:23:55 +0800

    添加导航栏

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Menu, Breadcrumb } from 'antd';
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Layout>
+                    <Header>
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={['2']}
+                            style={{ lineHeight: '64px' }}
+                        >
+                            <Menu.Item key="1">nav 1</Menu.Item>
+                            <Menu.Item key="2">nav 2</Menu.Item>
+                            <Menu.Item key="3">nav 3</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Layout>
+                        <Sider
+                            width={200}
+                            style={{ background: '#fff' }}
+                        >
+                            <Menu
+                                mode="inline"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                                style={{ height: '100%' }}
+                                >
+                                <Menu.Item key="1">系统基础信息</Menu.Item>
+                                <Menu.Item key="2">系统拓扑</Menu.Item>
+                                <Menu.Item key="3">域名查询</Menu.Item>
+                                <Menu.Item key="4">域名查询</Menu.Item>
+                            </Menu>
+                        </Sider>
+                        <Content>
+                            
+                        </Content>
+                    </Layout>
+                </Layout>
+            </div>  
+        );
+    }
 }
 
 export default App;
